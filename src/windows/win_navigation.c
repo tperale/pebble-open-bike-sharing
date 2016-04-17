5a61a09aa48edfcd423f7fc56c26afd419e16c0d
Perale Thomas <thomas.perale@openmailbox.org>
2016-04-17 17:36:19 +0200

    Pushed new image

--- src/windows/win_navigation.c
+++ src/windows/win_navigation.c
@@ -81,7 +81,7 @@ static void window_load(Window *window) {
     s_parking_bitmap = gbitmap_create_with_resource(RESOURCE_ID_PARKING_BITMAP);
     s_bicycle_bitmap_layer = bitmap_layer_create(GRect(bounds.size.w / 2, 0, (3 * bounds.size.w) / 4, bounds.size.h / 7));
     bitmap_layer_set_bitmap(s_parking_bitmap_layer, s_parking_bitmap);
-    /* layer_add_child(window_layer, bitmap_layer_get_layer(s_parking_bitmap_layer)); */
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_parking_bitmap_layer));
     /* TEXT : */
     s_text_layer_parking = text_layer_create(GRect((3 * bounds.size.w) / 4, 0, bounds.size.w, bounds.size.h / 7));
     text_layer_set_text_alignment(s_text_layer_parking, GTextAlignmentLeft);
