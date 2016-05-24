a1b586767099f6d769ee2a7db8e266ff664133e2
Perale Thomas <thomas.perale@openmailbox.org>
2016-05-24 23:49:45 +0200

    Change color when no bike left / no parking left

--- src/windows/station_info.c
+++ src/windows/station_info.c
@@ -14,13 +14,24 @@ static TextLayer* s_text_layer_parking = NULL;
 static char parkings_slots_buffer[8] = {};
 
 void update_station_info_with(uint32_t index) {
-    // Stations[index]
-    snprintf(free_bike_buffer, 8, "%ld", Stations[index].free_bike);
+    uint32_t free_bike = Stations[index].free_bike;
+    snprintf(free_bike_buffer, 8, "%ld", free_bike);
+    if (free_bike) {
+        text_layer_set_background_color(s_text_layer_free_bike, GColorWhite);
+    } else {
+        text_layer_set_background_color(s_text_layer_free_bike, GColorLightGray);
+    }
     text_layer_set_text(
             s_text_layer_free_bike,
             free_bike_buffer);
 
-    snprintf(parkings_slots_buffer, 8, "%ld", Stations[index].empty_slots);
+    uint32_t empty_slots = Stations[index].empty_slots;
+    snprintf(parkings_slots_buffer, 8, "%ld", empty_slots);
+    if (empty_slots) {
+        text_layer_set_background_color(s_text_layer_parking, GColorWhite);
+    } else {
+        text_layer_set_background_color(s_text_layer_parking, GColorLightGray);
+    }
     text_layer_set_text(
             s_text_layer_parking,
             parkings_slots_buffer);
@@ -42,7 +53,7 @@ void create_station_info(Layer* window_layer, GRect bounds) {
     layer_add_child(window_layer, bitmap_layer_get_layer(s_bicycle_bitmap_layer));
     /* TEXT : */
     s_text_layer_free_bike = text_layer_create(GRect(bounds.size.w / 4, bounds.size.h / 8, bounds.size.w / 4, bounds.size.h / 8));
-    text_layer_set_text_alignment(s_text_layer_free_bike, GTextAlignmentLeft);
+    text_layer_set_text_alignment(s_text_layer_free_bike, GTextAlignmentCenter);
     text_layer_set_background_color(s_text_layer_free_bike, GColorClear);
     layer_add_child(window_layer, text_layer_get_layer(s_text_layer_free_bike));
 
@@ -54,7 +65,7 @@ void create_station_info(Layer* window_layer, GRect bounds) {
     layer_add_child(window_layer, bitmap_layer_get_layer(s_parking_bitmap_layer));
     /* TEXT : */
     s_text_layer_parking = text_layer_create(GRect((3 * bounds.size.w) / 4, bounds.size.h / 8, bounds.size.w / 4, bounds.size.h / 8));
-    text_layer_set_text_alignment(s_text_layer_parking, GTextAlignmentLeft);
+    text_layer_set_text_alignment(s_text_layer_parking, GTextAlignmentCenter);
     text_layer_set_background_color(s_text_layer_parking, GColorClear);
     layer_add_child(window_layer, text_layer_get_layer(s_text_layer_parking));
 }
