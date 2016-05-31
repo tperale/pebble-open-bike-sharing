c5889cbbc855ee8ada0f887a3dccb4621cf8ef94
Perale Thomas <thomas.perale@openmailbox.org>
2016-06-01 01:00:58 +0200

    Segmenting the code to be easier to use.

---
+++ config/js/dev/pebble.js
@@ -0,0 +1,35 @@
+module.exports = {
+    getConfigData () {
+        let select = document.getElementById('stationList');
+        let apiAddress = select.options[select.selectedIndex].value;
+
+        console.log('Sending API address : ' + apiAddress + ' from ' + JSON.stringify(select.options));
+
+        let options = {
+        'api_address': apiAddress,
+        };
+
+        // Save for next launch
+        localStorage['api_address'] = options['api_address'];
+
+        //console.log('Got options: ' + JSON.stringify(options));
+        return options;
+    }
+
+    getQueryParam = (variable, defaultValue) {
+        let query = location.search.substring(1);
+        let vars = query.split('&');
+        for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        if (pair[0] === variable) {
+            return decodeURIComponent(pair[1]);
+        }
+        }
+        return defaultValue || false;
+    }
+
+    send () {
+        let return_to = getQueryParam('return_to', 'pebblejs://close#');
+        document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+    }
+};
