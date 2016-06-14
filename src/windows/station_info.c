63ad15728411dbb65b904551c20e7337969082fd
Perale Thomas <thomas.perale@openmailbox.org>
2016-06-14 20:43:01 +0200

    Adding lower level image manipulation.

--- src/windows/station_info.c
+++ src/windows/station_info.c
@@ -2,9 +2,9 @@
 #include "../globals.h"
 
 static GBitmap* s_bicycle_bitmap;
-static BitmapLayer* s_bicycle_bitmap_layer;
+static Layer* s_bicycle_bitmap_layer;
 static GBitmap* s_parking_bitmap;
-static BitmapLayer* s_parking_bitmap_layer;
+static Layer* s_parking_bitmap_layer;
 
 static Layer* s_direction_layer = NULL;
 static TextLayer* s_text_layer_free_bike = NULL;
@@ -12,6 +12,22 @@ static char free_bike_buffer[8] = {};
 
 static TextLayer* s_text_layer_parking = NULL;
 static char parkings_slots_buffer[8] = {};
+
+static void update_parking_layer (Layer *layer, GContext *ctx) {
+    GRect bounds = layer_get_bounds(layer);
+    graphics_context_set_compositing_mode(ctx, GCompOpSet);
+    graphics_context_set_antialiased(ctx, true);
+    /* graphics_draw_bitmap_in_rect(ctx, s_parking_bitmap, gbitmap_get_bounds(s_parking_bitmap)); */
+    graphics_draw_bitmap_in_rect(ctx, s_parking_bitmap, GRect(bounds.size.w / 2, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
+}
+
+static void update_bicycle_layer (Layer *layer, GContext *ctx) {
+    GRect bounds = layer_get_bounds(layer);
+    graphics_context_set_compositing_mode(ctx, GCompOpSet);
+    graphics_context_set_antialiased(ctx, true);
+    /* graphics_draw_bitmap_in_rect(ctx, s_bicycle_bitmap, gbitmap_get_bounds(s_bicycle_bitmap)); */
+    graphics_draw_bitmap_in_rect(ctx, s_bicycle_bitmap, GRect(0, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
+}
 
 void update_station_info_with(uint32_t index) {
     uint32_t free_bike = Stations[index].free_bike;
@@ -39,20 +55,24 @@ void update_station_info_with(uint32_t index) {
 
 void destroy_station_info() {
     gbitmap_destroy(s_bicycle_bitmap);
-    bitmap_layer_destroy(s_bicycle_bitmap_layer);
+    /* bitmap_layer_destroy(s_bicycle_bitmap_layer); */
+    layer_destroy(s_bicycle_bitmap_layer);
     gbitmap_destroy(s_parking_bitmap);
-    bitmap_layer_destroy(s_parking_bitmap_layer);
+    /* bitmap_layer_destroy(s_parking_bitmap_layer); */
+    layer_destroy(s_parking_bitmap_layer);
 }
 
 void create_station_info(Layer* window_layer, GRect bounds) {
     GFont s_font = fonts_get_system_font(FONT_KEY_GOTHIC_18);
     /* NUMBER OF FREE BIKE IN THE STATION */
     /* ICON : */
-    s_bicycle_bitmap_layer = bitmap_layer_create(GRect(0, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
+    /* s_bicycle_bitmap_layer = bitmap_layer_create(GRect(0, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8)); */
+    s_bicycle_bitmap_layer = layer_create(bounds);
+    layer_set_update_proc(s_bicycle_bitmap_layer, update_bicycle_layer);
     s_bicycle_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BICYCLE_BITMAP);
-
-    bitmap_layer_set_bitmap(s_bicycle_bitmap_layer, s_bicycle_bitmap);
-    layer_add_child(window_layer, bitmap_layer_get_layer(s_bicycle_bitmap_layer));
+    /* bitmap_layer_set_bitmap(s_bicycle_bitmap_layer, s_bicycle_bitmap); */
+    /* layer_add_child(window_layer, bitmap_layer_get_layer(s_bicycle_bitmap_layer)); */
+    layer_add_child(window_layer, s_bicycle_bitmap_layer);
     /* TEXT : */
     s_text_layer_free_bike = text_layer_create(GRect(bounds.size.w / 4, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
     text_layer_set_font(s_text_layer_free_bike, s_font);
@@ -62,10 +82,14 @@ void create_station_info(Layer* window_layer, GRect bounds) {
 
     /* NUMBER OF PARKING SLOTS */
     /* ICON : */
-    s_parking_bitmap_layer = bitmap_layer_create(GRect(bounds.size.w / 2, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
+    /* s_parking_bitmap_layer = bitmap_layer_create(GRect(bounds.size.w / 2, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8)); */
+    s_parking_bitmap_layer = layer_create(bounds);
+    layer_set_update_proc(s_parking_bitmap_layer, update_parking_layer);
+
     s_parking_bitmap = gbitmap_create_with_resource(RESOURCE_ID_PARKING_BITMAP);
-    bitmap_layer_set_bitmap(s_parking_bitmap_layer, s_parking_bitmap);
-    layer_add_child(window_layer, bitmap_layer_get_layer(s_parking_bitmap_layer));
+    /* bitmap_layer_set_bitmap(s_parking_bitmap_layer, s_parking_bitmap); */
+    /* layer_add_child(window_layer, bitmap_layer_get_layer(s_parking_bitmap_layer)); */
+    layer_add_child(window_layer, s_parking_bitmap_layer);
     /* TEXT : */
     s_text_layer_parking = text_layer_create(GRect((3 * bounds.size.w) / 4, (2 * bounds.size.h) / 8, bounds.size.w / 4, bounds.size.h / 8));
     text_layer_set_font(s_text_layer_parking, s_font);
